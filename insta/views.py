a200b6e6d30fbd55f5a22e1851f42e3449ce54aa
trevin-livele <liveletrevin6@gmail.com>
2022-03-07 23:01:26 +0300

    added user profile in home

--- insta/views.py
+++ insta/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from instagram .models import Post
-
+from accounts .models import Userprofile
 @login_required(login_url= 'login')
 def home(request):
+    userprofile = Userprofile.objects.get(user_id=request.user.id)
     post = Post.objects.all()
     context = {
         'posts' :  post,
+        'userprofile' : userprofile,
     }
     return render(request, 'home.html',context)
 
