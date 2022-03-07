a0a8ab5452570d69d18d99fd8ab8cd4a5f90bbf2
trevin-livele <liveletrevin6@gmail.com>
2022-03-07 23:37:36 +0300

    updated settings

--- insta/settings.py
+++ insta/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=e@r^^hdj7m001fn7xjehhiun(1zc)4t3xnq0hf-gdnwvfi$$&'
+SECRET_KEY = os.environ.get('secret_key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -150,8 +151,15 @@ MESSAGE_TAGS = {
 
 
 #SMTP CONFIGURATIONS
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'trevinlivele@gmail.com'
+# EMAIL_HOST_PASSWORD = 'ashihundu@2003'
+# EMAIL_USE_TLS = True
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'trevinlivele@gmail.com'
-EMAIL_HOST_PASSWORD = 'ashihundu@2003'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
